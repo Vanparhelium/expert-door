14eab4e692202eafa56fc3bd37266a05b380b92a
Vanparhelium <Vanparhelium@gmail.com>
2021-05-24 11:47:50 +0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+  // accordion
+$( function() {
+  $( "#accordion" ).accordion({
+    active: 'false',
+    collapsible: 'true',
+    heightStyle:  'content',
+    icons: 'false',
+  });
+  });
+});
